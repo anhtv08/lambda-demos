dd16b34a7dde50673b3bc5b2c675b421af669f59
anhtv08 <anhtv010888@gmail.com>
2019-06-27 16:57:07 +0800

    adding lambda functions

---
+++ s3_signature/s3_handlers.py
@@ -0,0 +1,82 @@
+import boto3
+import logging
+
+""" a simple example using lambda function to 
+  get event from s3 when new object created and created a pre-signed url to the object
+  once its completed then send a notification to sqs queue
+
+"""
+s3_client = boto3.client('s3')
+sqs_client = boto3.client('sqs')
+
+
+def create_presigned_url(s3_client, bucket_name, object_name, expirations=3600):
+    """Generate a presigned URL to share an S3 object
+
+        :type bucket_name: string
+        :param bucket_name
+        :type object_name : string
+        :param object_name
+        :param expiration: Time in seconds for the presigned URL to remain valid
+        :return: Presigned URL as string. If error, returns None.
+     """
+
+    try:
+        response = s3_client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': object_name
+            },
+            ExpiresIn=expirations
+        )
+
+        print("pre-signed-url: " + response)
+        return response
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+
+def send_sign_url_to_queue(sqs_client, queue_url, pre_signed_url):
+    """
+    :param queue_url: String
+    :param pre_signed_url: String
+    :return:
+    """
+    sqs_response = sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=pre_signed_url
+    )
+
+
+def upload_file_to_s3(s3_client, source_file, bucket_name):
+    try:
+        s3_client.upload_file(source_file, bucket_name)
+
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+
+def generate_signed_urls(records):
+    signed_url = []
+    for record in records:
+        bucket_name = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        val_presigned_url = create_presigned_url(
+            s3_client=s3_client,
+            bucket_name=bucket_name,
+            object_name=key
+        )
+        signed_url.append(val_presigned_url)
+
+
+def lambda_handler(event, context):
+    print(context)
+
+    # queue_url_endpoint = context['client_context']['evn']['queue_ur']
+    # print("passing queue_url from context:" + queue_url_endpoint)
+    records = event['Records']
+    return generate_signed_urls(records)
---
+++ tests/integration/integration_test.py
@@ -0,0 +1,91 @@
+from s3_signature import s3_handlers
+import unittest
+import botocore
+from botocore.stub import Stubber
+
+s3_client = botocore.session.get_session().create_client('s3')
+sqs_client = botocore.session.get_session().create_client('sqs')
+
+
+class TestHandlerS3SigningRequest(unittest.TestCase):
+
+    def setUp(self):
+        self.sampleInvokingEvent = {
+            "Records": [
+                {
+                    "eventVersion": "2.1",
+                    "eventSource": "aws:s3",
+                    "awsRegion": "us-west-2",
+                    "eventTime": "1970-01-01T00:00:00.000Z",
+                    "eventName": "event-type",
+                    "userIdentity": {
+                        "principalId": "Amazon-customer-ID-of-the-user-who-caused-the-event"
+                    },
+                    "requestParameters": {
+                        "sourceIPAddress": "ip-address-where-request-came-from"
+                    },
+                    "responseElements": {
+                        "x-amz-request-id": "Amazon S3 generated request ID",
+                        "x-amz-id-2": "Amazon S3 host that processed the request"
+                    },
+                    "s3": {
+                        "s3SchemaVersion": "1.0",
+                        "configurationId": "ID found in the bucket notification configuration",
+                        "bucket": {
+                            "name": "test-bucket",
+                            "ownerIdentity": {
+                                "principalId": "Amazon-customer-ID-of-the-bucket-owner"
+                            },
+                            "arn": "bucket-ARN"
+                        },
+                        "object": {
+                            "key": "test.txt",
+                            "size": 1024,
+                            "eTag": "object eTag",
+                            "versionId": "object version if bucket is versioning-enabled, otherwise null",
+                            "sequencer": "a string representation of a hexadecimal value used to determine event sequence",
+                        }
+                    },
+                    "glacierEventData": {
+                        "restoreEventData": {
+                            "lifecycleRestorationExpiryTime": "The time, in ISO-8601 format, for example, 1970-01-01T00:00:00.000Z, of Restore Expiry",
+                            "lifecycleRestoreStorageClass": "Source storage class for restore"
+                        }
+                    }
+                }
+            ]
+        }
+
+    def test_sign_s3_with_event_context(self):
+        # bucket_name = 'test-bucket'
+        # object_name = 'test.txt'
+        # expected_response = 'this_is_mocked_pre_signed_ulr'
+        # expected_params = {'bucket_name': 'test-bucket', 'object_name': 'test.txt'}
+        # expected_pre_signed_url = 'this_is_mocked_pre_signed_ulr'
+
+        s3_handlers.lambda_handler(
+            self.sampleInvokingEvent,
+            {}
+        )
+
+        # with Stubber(s3_client) as stubber:
+        #     stubber.add_response('generate_presigned_url', expected_response, expected_params)
+        #     stubber.activate()
+        #     result = s3_handlers.create_presigned_url(
+        #         s3_client,
+        #         bucket_name=bucket_name,
+        #         object_name=object_name
+        #
+        #     )
+        #     s3_handlers.lambda_handler(
+        #         self.sampleInvokingEvent,
+        #         {}
+        #
+        #     )
+        #
+        #     assert expected_pre_signed_url == result
+        #     stubber.deactivate()
+
+
+if __name__ == '__main__':
+    unittest.main()
