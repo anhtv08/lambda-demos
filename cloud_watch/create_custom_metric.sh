2be13c7e73d7c932b5fa268adc36056061208a28
anhtv08 <anhtv010888@gmail.com>
2019-07-06 08:14:50 +0800

    pumping metric to cloudwatch adding metric

--- cloud_watch/create_custom_metric.sh
+++ cloud_watch/create_custom_metric.sh
@@ -2,11 +2,16 @@
 
 aws_working_dir=$HOME/working/cloud-computing/lambda-demos
 
+for i in {1..100}; do
 
-echo "putting following metric data to cloud watch: $metric_data_test"
+echo "putting metric at index :$i"
 aws cloudwatch put-metric-data \
 --namespace  my-custom-metric-test \
---metric-data file://metric_data_file.json
+--metric-data file://metric_data_file.json &
+
+wait
+
+done
 
 
 
