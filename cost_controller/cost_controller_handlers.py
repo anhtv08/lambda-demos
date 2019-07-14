9cd5a3038a06d3af6a946a0141825d2755b52b8d
anhtv08 <anhtv010888@gmail.com>
2019-07-14 16:38:17 +0800

    code refactoring

--- cost_controller/cost_controller_handlers.py
+++ cost_controller/cost_controller_handlers.py
@@ -49,7 +49,7 @@ def shutdown_ec2_instance(ec2_client, instanceId):
 
 def validate_tag_name(tag: Dict[str, str]):
     required_tag_names: Sequence[str] = ['environment', 'owner', 'projectName', 'costCentre']
-    for item in tag:
+    for item in tag.keys():
         if item not in required_tag_names:
             return False
 
