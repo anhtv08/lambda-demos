ed6a948b3c8790f67c61f8c1d50b71c388c4f9c9
anhtv08 <anhtv010888@gmail.com>
2019-07-27 17:13:32 +0800

    adding anetha

--- scripts/my-vpc-flow-logs.sql
+++ scripts/my-vpc-flow-logs.sql
@@ -28,6 +28,59 @@ location 's3://j-my-vpc-flowlogs/AWSLogs/674028589551/vpcflowlogs/us-east-2/2019
 
 
 # create table on top of cloudtrail logs for querying data
+CREATE EXTERNAL TABLE IF NOT EXISTS cloudtrail_logs_table(
+eventversion STRING,
+useridentity STRUCT<
+       type:STRING,
+       principalid:STRING,
+       arn:STRING,
+       accountid:STRING,
+       accesskeyid:STRING,
+       userName:STRING,
+       sessioncontext:STRUCT<
+           attributes:STRUCT<
+               mfaauthenticated:STRING,
+               creationdate:STRING>,
+           invokedBy:STRING
+       >,
+        eventtime STRING,
+        eventsource STRING,
+        eventname STRING,
+        awsregion STRING,
+        sourceipaddress STRING,
+        useragent STRING,
+        requestparameters STRING,
+        responseelements STRING,
+        requestid STRING,
+        eventid STRING,
+        eventtype STRING,
+        recipientaccountid STRING,
+)
+
+
+us-east-2/2019/07/26
+
+PARTITIONED BY(
+    region string,
+    year string,
+    month string,
+    day string
+)
+ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/';
+
+# partition table
+
+ALTER TABLE vpc_flow_logs
+ADD PARTITION (
+    dt='yyyy-mm-dd'
+)
+location 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/us-east-2//2019/07/27';
+
+
+
 CREATE EXTERNAL TABLE cloudtrail_logs_table(
 eventversion STRING,
 useridentity STRUCT<
@@ -79,14 +132,17 @@ STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
 LOCATION 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/';
 
-# partition table
 
-ALTER TABLE vpc_flow_logs
+# replace with your specific settting
+# region, year, month, day
+
+ALTER TABLE cloudtrail_logs_table
 ADD PARTITION (
-    region=$var_region,
-    year=$var_year,
-    month=$var_month,
-    day=$var_day
+    region='us-east-2',
+    year=2019,
+    month=07,
+    day=27
 )
-location 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/$var_region//$var_year/$var_month/$var_day';
+location 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/us-east-2//2019/07/27';
+
 
