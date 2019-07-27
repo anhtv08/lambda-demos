e21061c46e93a4420499cb0300073d280b3e0e03
anhtv08 <anhtv010888@gmail.com>
2019-07-27 16:26:23 +0800

    adding vpc cloud trail tbl

--- scripts/my-vpc-flow-logs.sql
+++ scripts/my-vpc-flow-logs.sql
@@ -25,3 +25,68 @@ ALTER TABLE vpc_flow_logs
 ADD PARTITION (dt='YYYY-MM-dd')
 location 's3://j-my-vpc-flowlogs/AWSLogs/674028589551/vpcflowlogs/us-east-2/2019/07/26';
 
+
+
+# create table on top of cloudtrail logs for querying data
+CREATE EXTERNAL TABLE cloudtrail_logs_table(
+eventversion STRING,
+useridentity STRUCT<
+               type:STRING,
+               principalid:STRING,
+               arn:STRING,
+               accountid:STRING,
+               invokedby:STRING,
+               accesskeyid:STRING,
+               userName:STRING,
+sessioncontext:STRUCT<
+attributes:STRUCT<
+               mfaauthenticated:STRING,
+               creationdate:STRING>,
+sessionissuer:STRUCT<
+               type:STRING,
+               principalId:STRING,
+               arn:STRING,
+               accountId:STRING,
+               userName:STRING>>>,
+eventtime STRING,
+eventsource STRING,
+eventname STRING,
+awsregion STRING,
+sourceipaddress STRING,
+useragent STRING,
+errorcode STRING,
+errormessage STRING,
+requestparameters STRING,
+responseelements STRING,
+additionaleventdata STRING,
+requestid STRING,
+eventid STRING,
+resources ARRAY<STRUCT<
+               ARN:STRING,
+               accountId:STRING,
+               type:STRING>>,
+eventtype STRING,
+apiversion STRING,
+readonly STRING,
+recipientaccountid STRING,
+serviceeventdetails STRING,
+sharedeventid STRING,
+vpcendpointid STRING
+)
+PARTITIONED BY(region string,year string, month string, day string)
+ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/';
+
+# partition table
+
+ALTER TABLE vpc_flow_logs
+ADD PARTITION (
+    region=$var_region,
+    year=$var_year,
+    month=$var_month,
+    day=$var_day
+)
+location 's3://j-my-cloudtrail-logs/AWSLogs/674028589551/CloudTrail/$var_region//$var_year/$var_month/$var_day';
+
